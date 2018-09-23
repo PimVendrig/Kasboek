ff6a2f59435f6a872d23dce4c589c791385c2184
Pim Vendrig <github@pimvendrig.nl>
2018-09-24 00:52:29 +0200

    Bijwerken Migraties na upgrade naar Core 2.1

---
+++ Kasboek.WebApp/Data/Migrations/20180923224850_UpgradeNaar21.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Kasboek.WebApp.Data.Migrations
+{
+    public partial class UpgradeNaar21 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Bedrag",
+                table: "Transacties",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Bedrag",
+                table: "Transacties",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+        }
+    }
+}
